41e3af7c967ca2b54c151c76e54b52b2de6cb4a4
RobbLloyd90 <robbmorgan10@gmail.com>
2025-02-24 22:07:16 +0000

    updated the articleList to removed the ul tag and updated grid css styling

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,21 +2,19 @@ import { Link } from "react-router-dom";
 
 const ArticleList = ({ articles }) => {
   const listOfArticles = articles.map((article, index) => (
-    <ul key={index}>
-      <Link to={`/article/${article.article_id}`}>
-        <div className="article-card">
-          <img
-            key={index}
-            className="article-card-img"
-            src={article.article_img_url}
-            alt={article.title}
-          />
-          <div className="article-title-card">
-            <h3 className="container-text">{article.title}</h3>
-          </div>
+    <Link to={`/article/${article.article_id}`}>
+      <div className="article-card">
+        <img
+          key={index}
+          className="article-card-img"
+          src={article.article_img_url}
+          alt={article.title}
+        />
+        <div className="article-title-card">
+          <h3 className="container-text">{article.title}</h3>
         </div>
-      </Link>
-    </ul>
+      </div>
+    </Link>
   ));
   return <div className="rows">{listOfArticles}</div>;
 };
